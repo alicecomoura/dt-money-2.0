b412a2cfdc105a185ef4c03c93428c1945c4152a
Alice Coelho <alicecomoura@outlook.com>
2022-09-01 11:34:36 -0300

    transaction summary calculation function

--- src/components/summary/summary.tsx
+++ src/components/summary/summary.tsx
@@ -1,7 +1,35 @@
-import { SummaryContainer, SummaryCard } from './styles'
-import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from 'phosphor-react'
+import { useContext } from "react";
+import { TransactionsContext } from "../../contexts/transactionsContext";
+import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from "phosphor-react";
+
+import { SummaryContainer, SummaryCard } from "./styles";
 
 export function Summary() {
+  const { transactions } = useContext(TransactionsContext);
+
+  // o reduce neste caso transaforma o array de transactions em um objeto
+  // com os os totais => { income: 0, outcome: 0: total: 0 }
+  // dessa forma, é possível fazer a soma ou a subtração dos valores registrado
+
+  const summary = transactions.reduce(
+    (acc, transaction) => {
+      if (transaction.type === "income") {
+        acc.income += transaction.price;
+        acc.total += transaction.price;
+      } else {
+        acc.outcome += transaction.price;
+        acc.total -= transaction.price;
+      }
+
+      return acc;
+    },
+    {
+      income: 0,
+      outcome: 0,
+      total: 0,
+    }
+  );
+
   return (
     <SummaryContainer>
       <SummaryCard>
@@ -10,7 +38,7 @@ export function Summary() {
           <ArrowCircleUp size={32} color="#00b37e" />
         </header>
 
-        <strong>R$ 17.400,00</strong>
+        <strong>{summary.income}</strong>
       </SummaryCard>
 
       <SummaryCard>
@@ -19,7 +47,7 @@ export function Summary() {
           <ArrowCircleDown size={32} color="#f57a68" />
         </header>
 
-        <strong>R$ 17.400,00</strong>
+        <strong>{summary.outcome}</strong>
       </SummaryCard>
 
       <SummaryCard variant="green">
@@ -28,8 +56,8 @@ export function Summary() {
           <CurrencyDollar size={32} color="#fff" />
         </header>
 
-        <strong>R$ 17.400,00</strong>
+        <strong>{summary.total}</strong>
       </SummaryCard>
     </SummaryContainer>
-  )
+  );
 }
