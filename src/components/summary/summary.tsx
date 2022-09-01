0c1b87e5e33f17b2a9016175cab4b5c0b5bb50f7
Alice Coelho <alicecomoura@outlook.com>
2022-09-01 12:29:14 -0300

    custom hook to format values

--- src/components/summary/summary.tsx
+++ src/components/summary/summary.tsx
@@ -1,5 +1,4 @@
-import { useContext } from "react";
-import { TransactionsContext } from "../../contexts/transactionsContext";
+import { useSummary } from "../../hooks/useSummary";
 import { priceFormatter } from "../../utils/formatter";
 
 import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from "phosphor-react";
@@ -7,30 +6,7 @@ import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from "phosphor-react";
 import { SummaryContainer, SummaryCard } from "./styles";
 
 export function Summary() {
-  const { transactions } = useContext(TransactionsContext);
-
-  // o reduce neste caso transaforma o array de transactions em um objeto
-  // com os os totais => { income: 0, outcome: 0: total: 0 }
-  // dessa forma, é possível fazer a soma ou a subtração dos valores registrado
-
-  const summary = transactions.reduce(
-    (acc, transaction) => {
-      if (transaction.type === "income") {
-        acc.income += transaction.price;
-        acc.total += transaction.price;
-      } else {
-        acc.outcome += transaction.price;
-        acc.total -= transaction.price;
-      }
-
-      return acc;
-    },
-    {
-      income: 0,
-      outcome: 0,
-      total: 0,
-    }
-  );
+  const summary = useSummary();
 
   return (
     <SummaryContainer>
---
+++ src/hooks/useSummary.ts
@@ -0,0 +1,31 @@
+import { useContext } from "react";
+import { TransactionsContext } from "../contexts/transactionsContext";
+
+export function useSummary() {
+  const { transactions } = useContext(TransactionsContext);
+
+  // o reduce neste caso transaforma o array de transactions em um objeto
+  // com os os totais => { income: 0, outcome: 0: total: 0 }
+  // dessa forma, é possível fazer a soma ou a subtração dos valores registrado
+
+  const summary = transactions.reduce(
+    (acc, transaction) => {
+      if (transaction.type === "income") {
+        acc.income += transaction.price;
+        acc.total += transaction.price;
+      } else {
+        acc.outcome += transaction.price;
+        acc.total -= transaction.price;
+      }
+
+      return acc;
+    },
+    {
+      income: 0,
+      outcome: 0,
+      total: 0,
+    }
+  );
+
+  return summary;
+}
